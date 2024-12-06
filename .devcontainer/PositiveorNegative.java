56028819056b30bcaad2dd5adbe36433a9d5efa2
Abhiramsoma <114901769+Abhiramsoma@users.noreply.github.com>
2024-12-06 10:40:37 +0000

    container config done

---
+++ .devcontainer/PositiveorNegative.java
@@ -0,0 +1,17 @@
+public class PositiveorNegative {
+
+    public static void main(String[] args) {
+        int num=5;
+        if(num>=0){
+            if(num==0){
+                System.out.println("The number is a positive number");
+            }
+            else{
+                System.out.println("The number is a positive number");
+            }
+        }
+        else
+            System.out.println("The number is negative number ");
+        
+    }
+}
